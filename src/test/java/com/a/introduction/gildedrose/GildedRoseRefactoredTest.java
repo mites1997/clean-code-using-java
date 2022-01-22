1fcc05daebaeb152499754558248f754844adb66
mites1997 <arnabfeb15@gmail.com>
2022-01-22 11:33:25 +0530

    recfactor unit test for unexpired default item

--- src/test/java/com/a/introduction/gildedrose/GildedRoseRefactoredTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseRefactoredTest.java
@@ -1,147 +1,66 @@
 package com.a.introduction.gildedrose;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GildedRoseRefactoredTest {
-	
-	private static final int POSITIVE_SELLIN_LESS_THAN_5 = 2;
-	private static final int SELLIN_BETWEEN_5_AND_10 = 7;
-	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-	private static final int SELLIN_GREATER_THAN_10 = 15;
-	private static final int MAXIMUM_QUALITY = 50;
-	private static final String AGED_BRIE = "Aged Brie";
-	private static final int EXPIRED_SELLIN = -2;
-	private static final String DEFAULT_ITEM = "DEFAULT_ITEM";
-	private static final int DEFAULT_QUALITY = 4;
-	private static final int NOT_EXPIRED_SELLIN = 16;
 
-	@Test
-	public void unexpiredDefaultItem_qualityDecreasesBy1() {
-		
-		GildedRose app = createGildedRoseWithOneItem(DEFAULT_ITEM, 
-						NOT_EXPIRED_SELLIN, DEFAULT_QUALITY);
-		
-		app.updateQuality();
-		
-		Item expected = new Item(DEFAULT_ITEM, 
-						NOT_EXPIRED_SELLIN - 1,DEFAULT_QUALITY - 1);
-			
-		assertItem(expected, app.items[0]);
-	}
-	
-	@Test
-	public void expiredDefaultItem_qualityDecreasesBy2() {
-		
-		GildedRose app = createGildedRoseWithOneItem(DEFAULT_ITEM, 
-						EXPIRED_SELLIN , DEFAULT_QUALITY);
-		
-		app.updateQuality();
-		
-		Item expected = new Item(DEFAULT_ITEM, 
-				EXPIRED_SELLIN - 1, DEFAULT_QUALITY - 2);
-			
-		assertItem(expected, app.items[0]);
-	}
+	public static final int DEFAULT_QUALITY = 3;
+	public static final int DEFAULT_SELL_IN = 15;
+	public static final String DEFAULT_ITEM = "DEFAULT_ITEM";
 
+	/**
+	 * Method to test the variation in quality of the item for the non expired
+	 * Item.
+	 * 
+	 * The quality should decrease by 1 when the item is not expired
+	 * and sell in should decrease by 1.
+	 * 
+	 */
 	@Test
-	public void unexpiredAgedBrie_qualityIncreasesBy1() {
-		
-		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, 
-						NOT_EXPIRED_SELLIN , DEFAULT_QUALITY);
-		
+	public void testUnexpiredDefaultItemQualityDecreasesBy1() {
+		//setup
+		String defaultItem = DEFAULT_ITEM;
+		int notExpiredSellIn = DEFAULT_SELL_IN;
+		int defaultQuality = DEFAULT_QUALITY;
+		GildedRose app = createGildedRoseWithOneItem(defaultItem, notExpiredSellIn, defaultQuality);
+		//invoke
 		app.updateQuality();
-		
-		Item expected = new Item(AGED_BRIE, 
-				NOT_EXPIRED_SELLIN - 1, DEFAULT_QUALITY + 1 );
-			
-		assertItem(expected, app.items[0]);
-	}
 
-	@Test
-	public void expiredAgedBrie_qualityIncreasesBy2() {
-		
-		GildedRose app = 
-				createGildedRoseWithOneItem(AGED_BRIE, 
-						EXPIRED_SELLIN , DEFAULT_QUALITY);
-		
-		app.updateQuality();
-		
-		Item expected = new Item(AGED_BRIE, 
-			EXPIRED_SELLIN - 1, DEFAULT_QUALITY + 2 );
-			
-		assertItem(expected, app.items[0]);
-	}
-
-	@Test
-	public void unexpiredAgedBrie_qualityDoesNotGoBeyondMaximum() {
-		
-		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, 
-						NOT_EXPIRED_SELLIN , MAXIMUM_QUALITY);
-		
-		app.updateQuality();
-		
-		Item expected = new Item(AGED_BRIE, 
-				NOT_EXPIRED_SELLIN - 1, MAXIMUM_QUALITY );
-			
-		assertItem(expected, app.items[0]);
-	}
-
-	
-	@Test
-	public void backStagePassesBeyond10Days_qualityIncreasesBy1() {
-		
-		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES, 
-						SELLIN_GREATER_THAN_10 , DEFAULT_QUALITY );
-		
-		app.updateQuality();
-		
-		Item expected = new Item(BACKSTAGE_PASSES, 
-				SELLIN_GREATER_THAN_10 - 1, DEFAULT_QUALITY + 1 );
-			
-		assertItem(expected, app.items[0]);
-	}
-	
-	@Test
-	public void backStageBetween5And10Days_qualityIncreasesBy2() {
-		
-		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES, 
-						SELLIN_BETWEEN_5_AND_10 , DEFAULT_QUALITY );
-		
-		app.updateQuality();
-		
-		Item expected = new Item(BACKSTAGE_PASSES, 
-				SELLIN_BETWEEN_5_AND_10 - 1, DEFAULT_QUALITY +  2);
-			
-		assertItem(expected, app.items[0]);
-	}
-
-	@Test
-	public void backStageLessThan5Days_qualityIncreasesBy3() {
-		
-		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES, 
-						POSITIVE_SELLIN_LESS_THAN_5 , DEFAULT_QUALITY );
-		
-		app.updateQuality();
-		
-		Item expected = new Item(BACKSTAGE_PASSES, 
-				POSITIVE_SELLIN_LESS_THAN_5 - 1, DEFAULT_QUALITY +  3);
-			
+		//verify
+		Item expected=new Item(DEFAULT_ITEM,DEFAULT_SELL_IN-1,DEFAULT_QUALITY-1);
 		assertItem(expected, app.items[0]);
 	}
 
 	private void assertItem(Item expected, Item actual) {
 		assertEquals(expected.name, actual.name);
-		assertEquals(expected.sellIn, actual.sellIn);
-		assertEquals(expected.quality, actual.quality);
+		assertEquals(expected.sellIn , actual.sellIn);
+		assertEquals(expected.quality , actual.quality);
 	}
 
-	private GildedRose createGildedRoseWithOneItem(String itemType, int sellin, int quality) {
-		Item item = new Item(itemType, sellin, quality);
+	private GildedRose createGildedRoseWithOneItem(String defaultItem, int notExpiredSellIn, int defaultQuality) {
+		Item item = new Item(defaultItem, notExpiredSellIn, defaultQuality);
 		Item[] items = new Item[] { item };
 		GildedRose app = new GildedRose(items);
 		return app;
 	}
 
+	/**
+	 * Method to test the variation in quality of the item for the non expired
+	 * Item.
+	 * 
+	 * The quality should decrease by 2 when the item is expired(Sell in  < 0) and sell in should decrease by 1.
+	 * 
+	 */
+	@Test
+	public void testUpdateQualityForExpiredItem() {
+		Item item = new Item("DEFAULT_ITEM", -1, 3);
+		Item[] items = new Item[] { item };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals("DEFAULT_ITEM", app.items[0].name);
+		assertEquals(-2, app.items[0].sellIn);
+		assertEquals(1, app.items[0].quality);
+	}
 }
