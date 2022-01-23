991fe61cc7dab4d7671e7db20894e007adb199e9
mites1997 <arnabfeb15@gmail.com>
2022-01-23 18:22:52 +0530

    refactor tests

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -2,11 +2,32 @@ package com.b.simple.design.business.text;
 
 public class TextHelper {
 
-	public String swapLastTwoCharacters(String str) {
-		return null;
-	}
+    public String swapLastTwoCharacters(String str) {
+        int length = str.length();
 
-	public String truncateAInFirst2Positions(String str) {
-		return null;
-	}
+        if (length < 2) return str;
+
+        char lastChar = str.charAt(length - 1);
+
+        char secondLastChar = str.charAt(length - 2);
+
+        String restOfTheString = str.substring(0, length - 2);
+
+        return restOfTheString + lastChar + secondLastChar;
+
+
+    }
+
+    public String truncateAInFirst2Positions(String str) {
+		if(str.length()<2)
+			return str.replaceAll("A", "");
+
+		String first2Characters = str.substring(0, 2);
+
+		String first2CharactersUpdated = first2Characters.replaceAll("A", "");
+
+		String restOfTheString = str.substring(2);
+
+		return first2CharactersUpdated + restOfTheString;
+    }
 }
